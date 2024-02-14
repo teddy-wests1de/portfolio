ad136ae308d0484f638ebce3a9ce9e33059a5533
Elrico Landrew <dirkieys21@gmail.com>
2024-02-14 21:49:44 +0200

    Adding Mobile Menu Function

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-'use strict'
+"use strict";
+const nav = document.querySelector(".nav");
+const btnMenu = document.querySelectorAll(".btn-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+nav.addEventListener("click", function (e) {
+  const navLink = e.target.closest(".nav-link");
+  if (!navLink) return;
+  console.log(navLink);
+});
+
+btnMenu.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log(e.target);
+    navMenu.classList.toggle('hidden');
+  });
+});
